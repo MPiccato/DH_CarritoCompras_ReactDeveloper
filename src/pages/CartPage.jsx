d55cc02e618719d105a4c9e0daa393c0cb6eb753
MP Data - Trading <mpiccato@me.com>
2025-08-13 06:01:52 -0300

    carrito de compras en proceso

--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -1,7 +1,33 @@
-import React from 'react'
+import React from "react";
 
 export const CartPage = () => {
   return (
-    <div>CartPage</div>
-  )
-}
+    <>
+      <table class="table">
+        <thead>
+          <tr>
+            <th scope="col">Producto</th>
+            <th scope="col">Precio</th>
+            <th scope="col">Cantidad</th>
+            <th scope="col">Eliminar</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope="row">1</th>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+          </tr>
+        </tbody>
+      </table>
+
+      <div class="d-grid gap-2">
+        <button class="btn btn-primary" type="button">
+          Comprar
+        </button>
+       
+      </div>
+    </>
+  );
+};
